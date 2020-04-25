f09b4023446be263ff5b1928e7d83bf88a853cb6
Yuriy Korpachan <korpachan@gmail.com>
2020-04-25 18:42:51 +0300

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.Build();
+			var config = new TelegramBotConfig();
+			configuration.Bind("bot", config);
+
+			var Proxy = new WebProxy(config.Proxy.Host, config.Proxy.Port) { UseDefaultCredentials = true };
+			var botClient = new TelegramBotClient(config.Token, webProxy: Proxy);
+			var me = botClient.GetMeAsync().Result;
+			Console.WriteLine(
+				$"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+			);
+		}
+	}
+}
---
+++ src/TelegramBotConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot
+{
+	public class TelegramBotConfig
+	{
+		public string Token { get; set; }
+		public ProxyConfig Proxy { get; set; }
+	}
+
+	public class ProxyConfig
+	{
+		public string Host { get; set; }
+		public int Port { get; set; }
+	}
+}
